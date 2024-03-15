4ba294fd30fc46abe84ac6050a2a0568c5584edf
ilnurgaz <131668262+ilnurgaz@users.noreply.github.com>
2024-03-15 20:26:59 +0300

    Update not susceptible to os attack.php

--- Практическая 5/not susceptible to os attack.php
+++ Практическая 5/not susceptible to os attack.php
@@ -1,9 +1,19 @@
 <?php
-$filename = $_GET['filename']; 
+$filename = $_GET['filename'];
 
 if (preg_match('/^[a-zA-Z0-9_.]+$/', $filename)) {
-    system('ls ' . $filename);
+    $directory = "/path/to/directory/";
+
+    $full_path = $directory . $filename;
+
+    if (file_exists($full_path)) {
+        $output = shell_exec("ls " . escapeshellcmd($full_path));
+        echo "<pre>$output</pre>";
+    } else {
+        echo "Файл не найден!";
+    }
 } else {
     echo "Недопустимое имя файла!";
 }
 ?>
+
