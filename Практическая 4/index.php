83bb1a3a621fd86c5763372a83fc8171c34974ae
ilnurgaz <131668262+ilnurgaz@users.noreply.github.com>
2024-03-18 19:05:40 +0300

    Update index.php

--- Практическая 4/index.php
+++ Практическая 4/index.php
@@ -1,17 +1,29 @@
 <?php
 
-$allowed_ips = array("127.0.0.1", "456.456.456.456");
+function readIPsFromFile($filename) {
+    $ips = [];
+    if (file_exists($filename)) {
+        $ips = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    }
+    return $ips;
+}
+$white_list_file = "white.txt"; 
+$black_list_file = "black.txt"; 
 
+$white_list = readIPsFromFile($white_list_file);
+$black_list = readIPsFromFile($black_list_file);
 
 $user_ip = $_SERVER['REMOTE_ADDR'];
 
 echo $user_ip;
 
-if (in_array($user_ip, $allowed_ips)) {
+if (in_array($user_ip, $white_list)) {
+    echo "Content";
+} elseif (in_array($user_ip, $black_list)) {
     header("Location: error.php");
     exit();
+} else {
+    echo "Content";
 }
-
-echo "Content";
 
 ?>
